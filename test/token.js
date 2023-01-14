2579a524821becff3bd16c0e9a89cb5825bab4bd
muhammad-waqa <abdul1977ghaffar@gmail.com>
2023-01-14 16:00:20 +0500

    second test

--- test/token.js
+++ test/token.js
@@ -17,4 +17,22 @@ it("Deployement should assign total supply to owner",async function(){
     expect(await hardhatToken.supply()).to.equal(ownerbalance);
 
 });
+it("transfor tokens from one address to another",async function(){
+    //access account details balances or details,address of account
+    const [owner,address1,address2]= await ethers.getSigners();
+    const Token = await ethers.getContractFactory('Token');
+//deploying the contract.
+    const hardhatToken = await Token.deploy();
+
+
+//transfer 10 token from owner from address
+
+await hardhatToken.transer(address1.address,10);
+expect(await hardhatToken.balancesOf(address1.address)).to.equal(10);
+
+//transfer 5 of 10 token to other
+await hardhatToken.connect(address1).transer(address2.address,5);
+expect (await hardhatToken.balancesOf(address2.address)).to.equal(5);
+});
+
 });
